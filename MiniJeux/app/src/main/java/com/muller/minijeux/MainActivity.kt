2ac5214328314d1301d24374831be9d541c00037
Muller Jimmy <JimmyMullerGuirdin@gmail.com>
2020-11-07 13:53:08 +0100

    Fonctionnalité Start
    Modification du nom accueil.xml en home.xml
    Modification du nom FondGris en fond_gris
    Création MorpionActivity
    Création fonction VerifJoueur
    Ajout fonction onChange pour les EditText
    
    Finnalisation de la fonctionnalité "Start"

--- MiniJeux/app/src/main/java/com/muller/minijeux/MainActivity.kt
+++ MiniJeux/app/src/main/java/com/muller/minijeux/MainActivity.kt
@@ -1,11 +1,59 @@
 package com.muller.minijeux
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import kotlinx.android.synthetic.main.home.*
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    lateinit var EditNamePlayer1:EditText
+    lateinit var EditNamePlayer2:EditText
+    lateinit var ButtonStart: Button
+
+    fun VerifNomJoueur() // Permet la désactivation du bouton start, si les nom des joueurs ne sont pas saisie
+    {
+        if (!(EditNamePlayer1.text.isEmpty()) && !(EditNamePlayer2.text.isEmpty()))
+        {
+            buttonStart.isEnabled = true
+        }
+    }
+
+    fun EditText.onChange () //Permet de vérifier si nos EditText sont remplis et s'ils le sont bien
+    {
+        this.addTextChangedListener(object: TextWatcher
+        {
+            override fun afterTextChanged(s: Editable?) { VerifNomJoueur()  }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.acceuil)
+        setContentView(R.layout.home)
+
+        // Liaison des variables aux éléments xml
+        EditNamePlayer1 = findViewById(R.id.editNamePlayer1)
+        EditNamePlayer2 = findViewById(R.id.editNamePlayer2)
+        ButtonStart  = findViewById(R.id.buttonStart)
+
+        // Verification des EditText
+        EditNamePlayer1.onChange()
+        EditNamePlayer2.onChange()
+
+        // Passe à l'activité suivante avec les noms des joueurs
+        ButtonStart.setOnClickListener {
+            val monIntent :Intent = Intent(this,MorpionActivity::class.java).apply {
+                putExtra("Joueur1",EditNamePlayer1.text.toString())
+                putExtra("Joueur2",EditNamePlayer2.text.toString())
+            }
+            startActivity(monIntent)
+        }
     }
 }
---
+++ MiniJeux/app/src/main/java/com/muller/minijeux/MorpionActivity.kt
@@ -0,0 +1,14 @@
+package com.muller.minijeux
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MorpionActivity: AppCompatActivity()
+{
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.morpion)
+    }
+}
